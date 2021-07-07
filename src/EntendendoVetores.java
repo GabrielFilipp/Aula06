befb3d09ae0a43a648b53f0cb51c1cbe7b002fae
Gabriel Filipp <filipp278@gmail.com>
2021-07-07 19:11:28 -0300

    Entendendo Vetores

---
+++ src/EntendendoVetores.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class EntendendoVetores {
+
+	public static void main(String[] args) {
+
+		Scanner leitor = new Scanner(System.in);
+		int vetor[] = new int[5];
+
+		vetor[0] = 50;
+		vetor[1] = 30;
+		vetor[2] = 70;
+		vetor[3] = 90;
+		vetor[4] = 70;
+
+		for (int i = 0; i < 5; i++) {
+			System.out.println("Informe um novo valor para a posição [" + i + "] do vetor:");
+			vetor[i] = leitor.nextInt();
+		}
+
+		for (int i = 0; i < 5; i++) {
+			System.out.println("a posição ["+i+"] do vetor tem o valor : "+vetor[i]);
+		}
+
+		leitor.close();
+	}
+
+}
